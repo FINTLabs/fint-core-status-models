d60ca2e180c9734a4d7e8e550879ee3cd012fd9c
Hknots <hknots99@gmail.com>
2025-03-21 10:35:23 +0100

    initial commit

---
+++ src/test/kotlin/no/fintlabs/core/status/service/FintCoreStatusServiceLibraryApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.fintlabs.core.status.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FintCoreStatusServiceLibraryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
