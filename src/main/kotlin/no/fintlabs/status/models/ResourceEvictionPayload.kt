f8d54e666c9f1892cb9cd496e519e81f722f8cdb
Hknots <hknots99@gmail.com>
2025-03-25 12:15:28 +0100

    refactor: add default constructor

--- src/main/kotlin/no/fintlabs/status/models/ResourceEvictionPayload.kt
+++ src/main/kotlin/no/fintlabs/status/models/ResourceEvictionPayload.kt
@@ -1,9 +1,9 @@
 package no.fintlabs.status.models
 
 data class ResourceEvictionPayload(
-    val domain: String,
-    val pkg: String,
-    val resource: String,
-    val org: String,
+    val domain: String = "",
+    val pkg: String = "",
+    val resource: String = "",
+    val org: String = "",
     val unixTimestamp: Long = System.currentTimeMillis()
 )
